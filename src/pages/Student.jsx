a17dda3572e2a4e5ed14bdc6a912781b002cb35d
Abdullah230102 <abdullahbhatti8776@gmail.com>
2025-10-01 17:45:04 +0500

    ......

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -11,7 +11,7 @@ const Students = () => {
   ];
 
   return (
-    <div className="container mx-auto px-4 py-10">
+    <div className="container mx-auto px-5 py-10">
       <h1 className="text-3xl font-extrabold text-center text-blue-700 mb-10">
         Our Students
       </h1>
